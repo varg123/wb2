131f12bb8c43d1f614b1fb9764b83aa8d0cdcd5f
Andrey Skilev <andreyskilev@mail.ru>
2021-08-17 18:45:07 +0500

    refactor singleton in appconfig

--- appconfig/appconfig.py
+++ appconfig/appconfig.py
@@ -1,5 +1,3 @@
-# todo: сделать сингтон чтобы убрать постоянную загрузку словаря
-
 import os
 import json
 
@@ -7,10 +5,20 @@ import json
 class AppConfig:
     _config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.json')
     _data = {}
+    __instance = None
 
     def __init__(self):
-        with open(self._config_path, 'rt', encoding='utf-8') as config_json:
-            self._data = json.load(config_json)
+        if not AppConfig.__instance:
+            with open(self._config_path, 'rt', encoding='utf-8') as config_json:
+                self._data = json.load(config_json)
+                AppConfig.__instance = self
 
-    def get(self, param):
-        return self._data.get(param)
+    @classmethod
+    def get(cls, param):
+        return cls.__instance._data.get(param)
+
+    @classmethod
+    def getInstance(cls):
+        if not cls.__instance:
+            cls.__instance = AppConfig()
+        return cls.__instance
